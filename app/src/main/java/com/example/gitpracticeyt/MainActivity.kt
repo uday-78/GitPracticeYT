8186db925286f3c708d40b691d6f848202040ffa
uday-78 <udaytewary54@gmail.com>
2023-03-06 12:28:47 +0530

    This is our third commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.gitpracticeyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
+        today is our good day
     }
 }
